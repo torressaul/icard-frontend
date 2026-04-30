2b12d9a9a59a7034a685cc6a12d0d86f56462446
Saul Yair <torressaul1001@gmail.com>
2026-04-30 17:34:51 -0600

    Primer commit: listo para Netlify

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const BASE_API = "https://mi-icard-backend-7c3c2cd0de47.herokuapp.com";
+export const TOKEN = "token";
+
+export const ORDER_STATUS = {
+  PENDING: "PENDING",
+  DELIVERED: "DELIVERED",
+};
+
+export const PAYMENT_STATUS = {
+  PENDING: "PENDING",
+  PAID: "PAID",
+};
